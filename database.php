0d2feb3448f543850bb8115fe65751d0130206d4
mikael308 <mikael.holmbom@hotmail.com>
2016-11-06 23:31:26 +0100

    database add func delete::forumpost

--- database.php
+++ database.php
@@ -500,6 +500,26 @@
 			}
 			return False;
 		}
+		/**
+		 * deletes forumpost from database
+		 *
+		 * @param post post to delete
+		 * @return True if deletion was successful
+		 */
+		public static function forumPost(ForumPost $post){
+			$db_conn = connect();
+			if ($db_conn){
+				$query = "DELETE FROM " . $GLOBALS['dbtable_forumposts'] . " as fpost "
+					. " WHERE fpost.id='" . $post->getPrimaryKey() . "';";
+				$res = pg_query($db_conn, $query);
+				if($res){
+					pg_free_result($res);
+					return True;
+				}
+			}
+			return False;
+		}
+		
 	} # !DELETE
 
 
