a7eaec9705db2f7f42a0a640c66d0e15165aeafe
mikael308 <mikael.holmbom@hotmail.com>
2016-11-06 21:00:08 +0100

    forumformat_thread: add edit post form

--- forumformat_thread.php
+++ forumformat_thread.php
@@ -5,6 +5,7 @@
  */
 
  	require_once "forumformat.php";
+	require_once "listeners.php";
 
 	/**
 	 * display forumcontent list of posts
@@ -47,6 +48,8 @@
 		if ($post == NULL) 
 			return errorMessage("could not display post");
 		
+		$editMessageForm =  editable($post)? editMessageForm($post) : "";
+		
 		$author = $post->getAuthor();
 		return '<div class="forum_content_listitem forum_post">'
 			. '<div class="author">'
@@ -55,10 +58,45 @@
 				. '<div class="registered">registered<br>'.formatDate($author->getRegistered()).'</div>'
 			. '</div>'
 			. '<div class="post">'
+			. 	'<div class="option_panel">' 
+			.		$editMessageForm
+			.	'</div>'	
 				. '<div class="message">' . $post->getMessage() . '</div>'
 				. '<div class="created">created:' . $post->getCreated() . '</div>'
 			. '</div>'
 		. '</div>';
+	}
+	
+	function editable($post){
+		if($post == null)
+			return False;
+		
+		$authUser = getAuthorizedUser();
+		if($authUser->isAdmin() || $authUser->isModerator())
+			return True;
+		
+		# if users message and created within 15min
+		if($authUser->getName() == $post->getAuthor()->getName()){
+			$a = date($GLOBALS['timestamp_format']);
+			$b = $post->getCreated();
+		
+			$diff = strtotime($a) - strtotime($b);
+			if($diff < (60 * 15))
+				return True;
+		
+			
+		}
+		
+		return False;
+	}
+	
+	function editMessageForm(ForumPost $post){
+		return 
+			  '<form class="edit_post" name="" method="POST" action="'.htmlspecialchars("post_page.php").'" >'
+			.		'<input type="hidden" name="post" value="' . $post->getPrimaryKey() . '" />'
+			.		'<input type="hidden" name="msg" value="' . $post->getMessage() . '" />'
+			.		'<input type="submit" class="material-icons" value="edit" name="edit_post"></input>' 
+			. '</form>';
 	}
 	
 
