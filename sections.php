4752eceb7fabb47cfd891eacdfc17ced51e0a032
mikael308 <mikael.holmbom@hotmail.com>
2016-11-11 13:40:19 +0100

    sections: remove autofocus on searchbar input

--- sections.php
+++ sections.php
@@ -231,7 +231,7 @@
 			. "<input type='radio' value='post' name='search_type' checked >"
 			. "<label for='user'>user</label>"
 			. "<input type='radio' value='user' name='search_type' ><br>"
-			. "<input type='text' id='searchbar' name='search_value' onkeyup='search(this.value)' autocomplete='off' autofocus >"
+			. "<input type='text' id='searchbar' name='search_value' onkeyup='search(this.value)' autocomplete='off' >"
 			. "<input type='submit' value='search'>" #TODO kolla hur du kan sätta den som search icon button istället
 		. "</form>";
 	}
