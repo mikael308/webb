68e58171a97d2eaf7e18ed14c0e9ca8d1ac20ed8
mikael308 <mikael.holmbom@hotmail.com>
2017-02-18 19:15:40 +0100

    sidepanel use icon search button

--- sections.php
+++ sections.php
@@ -236,10 +236,11 @@
 	 *
 	 */
 	function getSearchForm(){
+
 		return
 		"<form method='GET' id='searchform'>"
 			. "<input type='text' id='searchbar' name='search_value' onkeyup='search(this.value)' autocomplete='off' >"
-			. "<input type='submit' value='search'>" #TODO kolla hur du kan sätta den som search icon button istället
+			. "<input id='searchform_btn' type='submit' value='search' class='clickable material-icons'>"
 			. "<br>"
 			. "<label for='post'>post</label>"
 			. "<input type='radio' value='post' name='search_type' onclick='search(search_value.value)' checked >"
