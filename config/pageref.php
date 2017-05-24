eb69ed127b5c5f72f9cb95b8870ac79ea65e53b9
mikael308 <mikael.holmbom@hotmail.com>
2017-05-24 09:18:31 +0200

    rename pages

--- config/pageref.php
+++ config/pageref.php
@@ -6,12 +6,12 @@
  */
 
 	$GLOBALS['index_page'] = "index.php";
-	$GLOBALS['post_page'] = "post_page.php";
-	$GLOBALS['forum_page'] = "forum_page.php";
-	$GLOBALS['admin_page'] = "admin_page.php";
-	$GLOBALS['user_page'] = "viewuser_page.php";
-	$GLOBALS['about_page'] = "about_page.php";
-	$GLOBALS['register_page'] = "registeruser_page.php";
-	$GLOBALS['contact_page'] = "contact_page.php";
+	$GLOBALS['post_page'] = "post.php";
+	$GLOBALS['forum_page'] = "forum.php";
+	$GLOBALS['admin_page'] = "admin.php";
+	$GLOBALS['user_page'] = "user.php";
+	$GLOBALS['about_page'] = "about.php";
+	$GLOBALS['register_page'] = "registeruser.php";
+	$GLOBALS['contact_page'] = "contact.php";
 
 ?>
