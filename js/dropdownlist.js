239adc3cf3ff3cbf6c4eabc098c12940885b3d5e
mikael308 <mikael.holmbom@hotmail.com>
2017-05-24 11:54:28 +0200

    fix javadoc format

--- js/dropdownlist.js
+++ js/dropdownlist.js
@@ -1,11 +1,11 @@
 
 /**
-
-add functions to all elements of class dropbtn:
-make related dropdown-content visible 
-
-@author Mikael Holmbom
-*/
+ *
+ * add functions to all elements of class dropbtn:
+ * make related dropdown-content visible 
+ * 
+ * @author Mikael Holmbom
+ */
 
 
 var ddbtns = document.getElementsByClassName("dropbtn");
