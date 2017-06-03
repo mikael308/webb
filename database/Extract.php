af0b1b81675e22ac7d676300ebd2d28d61c1a6c5
mikael308 <mikael.holmbom@hotmail.com>
2017-06-03 18:10:37 +0200

    init

---
+++ database/Extract.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * extract data from database
+ * @author Mikael Holmbom
+ * @version 1.0
+ */
+
+	class Extract{
+
+		static function latestThreads(int $amount){
+			$latestThreads=array();
+			$db_conn = connect();
+			if ($db_conn){
+
+				$query = " SELECT *"
+				. " FROM ".$GLOBALS['dbtable_forumthreads']." AS t"
+				. " WHERE t.id IN "
+				. " ("
+				.  " SELECT p.thread"
+				.  " FROM ".$GLOBALS['dbtable_forumposts']." AS p"
+				.  " GROUP BY p.thread"
+				.  " ORDER BY MAX(p.created) DESC"
+				.  " LIMIT($amount)"
+				. " )"
+				. " ;";
+
+				$res = pg_query($db_conn, $query);
+				if($res){
+					for($i = 0; $i < pg_num_rows($res); $i++){
+						$latestThreads[] = Parse::toThread(pg_fetch_object($res, $i));
+					}
+					pg_free_result($res);
+				} else {
+					throw new RuntimeException(pg_last_error($db_conn));
+				}
+
+			}
+			return $latestThreads;
+		}
+	}
+
+?>
