e60a4f20dbaf8e56c195f8e35b38cf026b783e36
mikael308 <mikael.holmbom@hotmail.com>
2017-06-03 14:27:11 +0200

    init storedprocedures sql script

---
+++ config/setup/setup_storedprocedures.sql
@@ -0,0 +1,46 @@
+
+---------------------------------------------------------------------------------------
+--
+-- STORED PROCEDURES
+--
+---------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION proj.get_user(IN p_name text DEFAULT '%'::text)
+ RETURNS TABLE(
+  name TEXT,
+  email TEXT,
+  role INTEGER,
+  banned boolean,
+  registered timestamp
+ ) AS $$
+ BEGIN
+  RETURN QUERY SELECT
+  u.name,
+  u.email, 
+  CAST(u.role AS INTEGER),
+  CAST(u.banned AS BOOLEAN),
+  u.registered
+  FROM proj.forumusers as u
+  WHERE u.name LIKE p_name;
+ END; $$
+
+LANGUAGE PLPGSQL;
+ 
+CREATE OR REPLACE FUNCTION proj.get_post(IN p_msg TEXT DEFAULT '%'::text )
+ RETURNS TABLE(
+  author TEXT,
+  thread INTEGER,
+  id INTEGER,
+  message TEXT,
+  created TIMESTAMP,
+  edited TIMESTAMP
+ ) AS $$
+
+ BEGIN
+  RETURN QUERY SELECT
+  *
+  FROM proj.forumposts AS p
+  WHERE p.message LIKE p_msg;
+ END; $$
+ 
+ LANGUAGE PLPGSQL;
