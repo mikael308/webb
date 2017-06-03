a5f00170b742675a6489bcb3bb690e564290e3bd
mikael308 <mikael.holmbom@hotmail.com>
2017-06-03 14:28:27 +0200

    update setup script procedure

--- config/setup/setup.sh
+++ config/setup/setup.sh
@@ -1,4 +1,7 @@
+psql -U mikael -d postgres -a -f ./config/setup/setup_tables.sql
+psql -U mikael -d postgres -a -f ./config/setup/init_roles.sql
 
-exec psql -U mikael -d postgres -a -f setup_tables.sql
-exec php setup_users.php
-exec psql init_vals.sql
+php ./config/setup/setup_users.php
+
+psql -U mikael -d postgres -a -f ./config/setup/init_vals.sql
+psql -U mikael -d postgres -a -f ./config/setup/setup_storedprocedures.sql
