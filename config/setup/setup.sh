9266ed71d9dadfc845af72a75c0aafb6575a7b5c
mikael308 <mikael.holmbom@hotmail.com>
2017-06-03 18:45:03 +0200

    add setup_forumposts

--- config/setup/setup.sh
+++ config/setup/setup.sh
@@ -5,3 +5,4 @@ php ./config/setup/setup_users.php
 
 psql -U mikael -d postgres -a -f ./config/setup/init_vals.sql
 psql -U mikael -d postgres -a -f ./config/setup/setup_storedprocedures.sql
+php ./config/setup/setup_forumposts.php
