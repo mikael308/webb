941c4aab30fe47e51ab5066395c19b422edaac91
mikael308 <mikael.holmbom@hotmail.com>
2017-06-03 18:43:45 +0200

    init sections/information

---
+++ sections/information.php
@@ -0,0 +1,34 @@
+<?php
+	/**
+	 * sections containing information about forum content
+	 * @author Mikael Holmbom
+	 * @version 1.0
+	 */
+
+	require_once "./database/Extract.php";
+
+	function threadView(ForumThread $thread){
+		return
+			'<a href="forum.php?t='.$thread->getId().'&p='.$thread->getLastPageIndex().'" >'
+			. '<div>'
+			.		$thread->getTopic()
+			. " : "
+			. 	$thread->getLastAttributor()->getName()
+			. '</div>'
+			. '</a>';
+	}
+
+	function displayLatestThreads(int $amount){
+		$threadscont="";
+		$threads=Extract::latestThreads($amount);
+		foreach($threads as $thread){
+			$threadscont .= threadView($thread);
+		}
+		return
+			'<div id="latestThreads">'
+			. 	$threadscont
+			. '</div>';
+
+	}
+
+?>
