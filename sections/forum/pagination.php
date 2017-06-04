92fcf49de092c62a246bed2146f748a23931846c
mikael308 <mikael.holmbom@hotmail.com>
2017-06-04 16:58:48 +0200

    update pagination

--- sections/forum/pagination.php
+++ sections/forum/pagination.php
@@ -1,79 +1,171 @@
 <?php
-
 	/**
-	 * get pagination button
-	 * @param $link ref
-	 * @param $idx index
-	 * @param $class optional class
-	 * @return button as html
+	 * object used to generate pagination
+	 * use generateSubject, generateThread
+	 * @author Mikael Holmbom
+	 * @version 1.0
 	 */
-	function pagButton($link, $idx, $class=""){
-		return
-			'<a class="pag_button pag_button_dir '.$class.'" href="'. $link . '">'
-		.		$idx
-		. '</a>';
-	}
 
-	/**
-	 * get the pagination indexes buttons
-	 * @param currentPage the current displayed page
-	 * @param pageWidth the interval amount of pages adjacent to current page
-	 * @param n_pages total number of pages
-	 * @param link the page pagination button will direct to, without the page value
-	 * @return content as html string
-	 */
-	function pagination($currentPage, $pageWidth, $n_pages, $link){
-		if ($currentPage == NULL || $link == NULL) return "";
-		$pageWidth = ceil($pageWidth);
+	 class Pagination {
 
-		# offset of pagination indexes
-		$left_offset 	= $currentPage - $pageWidth;
-		$right_offset 	= $currentPage + $pageWidth;
+		 /**
+		  * generate pagination for subject
+			* @param $subject
+			* @param currentPage the current displayed page
+		  * @return as html
+			*/
+		 public static function generateSubject(ForumSubject $subject, $currentPage) {
+			 $link = $GLOBALS['forum_page'] . "?s=" . $subject->getPrimaryKey();
+			 $max_pages = Count::maxPagesSubject($subject);
+			 return Pagination::generate(
+				 $subject->getPrimaryKey(),
+				 $currentPage,
+				 $max_pages,
+				 $link
+			 );
+		 }
 
-		# expand the upper or lower interval if current page
-		# is in beginning or end of page list length
-		if($left_offset < 1){
-			$right_offset = min($n_pages, ($right_offset+ abs($left_offset-1)));
-		}
-		if($right_offset > $n_pages){
-			$left_offset = max(1, ($left_offset - abs($n_pages-$right_offset)));
-		}
+ 		 /**
+ 		  * generate pagination for thread
+ 			* @param $thread
+ 			* @param currentPage the current displayed page
+ 		  * @return as html
+ 			*/
+		 public static function generateThread(ForumThread $thread, $currentPage) {
+			 $link = $GLOBALS['forum_page'] . "?t=" . $thread->getPrimaryKey();
+			 $max_pages = Count::maxPagesThread($thread);
+			 return Pagination::generate(
+				 $thread->getPrimaryKey(),
+				 $currentPage,
+				 $max_pages,
+				 $link
+			 );
+		 }
 
-		# correct the interval to match the size of the page list length
-		$i 		= max($left_offset, 1); # start offset -> min value: 1
-		$maxlim = min($right_offset, $n_pages);
-		if($maxlim < 2){
-			return "";
-		}
-		# page index
-		$pagIdxBtns = "";
-		for(; $i <= $maxlim; $i++){
-			$class = "";
-			if($i == $currentPage){
-				$class = " pag_button_current";
-			}
-			$pagIdxBtns .= pagButton($link."&p=".$i, $i, $class);
-		}
+		 	/**
+		 	 * get the pagination indexes buttons
+			 * @param $id index of the object to display its content
+		 	 * @param currentPage the current displayed page
+		 	 * @param max_pages total number of pages
+		 	 * @param link the page pagination button will direct to, without the page value
+		 	 * @return content as html string
+		 	 */
+		 	private static function generate($id, $currentPage, $max_pages, $link){
+		 		if ($id == NULL || $currentPage == NULL
+					|| $max_pages == NULL || $link == NULL){
+		 			return "";
+		 		}
 
-		$prevPage 	= max($currentPage-1, 1);
-		$nextPage 	= min($currentPage+1, $maxlim);
+		 		$offset = Pagination::getOffsets($currentPage, $max_pages);
 
-		return
-			'<div id="pag_nav">'
-			. 	pagButton(
-					$link."&p=1",
-					"<i id='pag_first' class='material-icons'>first_page</i>")
-			. 	pagButton(
-					$link."&p=".$prevPage,
-					"<i id='pag_prev'  class='material-icons'>navigate_before</i>")
-			. 	$pagIdxBtns
-			. 	pagButton(
-					$link."&p=".$nextPage,
-					"<i id='pag_next'  class='material-icons'>navigate_next</i>")
-		 	. 	pagButton(
-		 			$link."&p=".$n_pages,
-		 			"<i id='pag_last'  class='material-icons'>last_page</i>")
-			. '</div>';
-	}
+		 		# correct the interval to match the size of the page list length
+		 		$i = max($offset["left"], 1); # start offset -> min value: 1
+		 		$maxlim = min($offset["right"],	$max_pages);
+
+		 		# generate index buttons html
+		 		$pagIdxBtns = "";
+		 		for(; $i <= $maxlim; $i++){
+		 			$class = "";
+		 			if($i == $currentPage){
+		 				$class = " pag_button_current";
+		 			}
+		 			$pagIdxBtns .= Pagination::pagButton($link."&p=".$i, $i, $class);
+		 		}
+
+		 		return
+		 			'<div id="pag_nav">'
+		 			. 	Pagination::navButtonLeft($link, $currentPage)
+		 			. 	$pagIdxBtns
+		 			. 	Pagination::navButtonRight($link, $currentPage, $maxlim, $max_pages)
+		 			. '</div>';
+		 	}
+
+			/**
+		 	 * get pagination button
+		 	 * @param $link ref
+		 	 * @param $idx index
+		 	 * @param $class optional class
+		 	 * @return button as html
+		 	 */
+		 	private function pagButton($link, $idx, $class=""){
+		 		return
+		 			'<a class="pag_button pag_button_dir '.$class.'" href="'. $link . '">'
+		 		.		$idx
+		 		. '</a>';
+		 	}
+
+		 	/**
+		 	 * get navigation button to previous or first page
+		 	 * @return as html
+		 	 */
+		 	private function navButtonLeft($link, $currentPage){
+		 		$prevPage = max($currentPage-1, 1);
+		 		return
+		 			# nav button to first page
+		 			Pagination::pagButton(
+		 				$link."&p=1",
+		 				"<i id='pag_first' class='material-icons'>first_page</i>"
+		 			)
+		 			# nav button to previous page
+		 			.	Pagination::pagButton(
+		 				$link."&p=".$prevPage,
+		 				"<i id='pag_prev'  class='material-icons'>navigate_before</i>"
+		 			);
+		 	}
+
+		 	/**
+		 	 * get navigation button to next or last page
+		 	 * @return as html
+		 	 */
+		 	private function navButtonRight($link, $currentPage, $maxlim, $n_pages){
+		 		$nextPage = min($currentPage+1, $maxlim);
+		 		return
+		 			# nav button to next page
+		 			Pagination::pagButton(
+		 				$link."&p=".$nextPage,
+		 				"<i id='pag_next' class='material-icons'>navigate_next</i>"
+		 			)
+		 			# nav button to last page
+		 			. Pagination::pagButton(
+		 				$link."&p=".$n_pages,
+		 				"<i id='pag_last' class='material-icons'>last_page</i>"
+		 			);
+		 	}
+
+		 	/**
+		 	 * get the index offsets of pagination
+		 	 * the interval of page links will always be max value of
+		 	 * setting value [pag_max_interval]
+		 	 * @return as html
+		 	 */
+		 	private function getOffsets($currentPage, $n_pages){
+		 		$pageWidth = (int) readSettings("pag_max_interval");
+
+		 		# offset of pagination indexes
+		 		$left_offset = $currentPage - $pageWidth;
+		 		$right_offset = $currentPage + $pageWidth;
+
+		 		# expand the upper or lower interval if current page
+		 		# is in beginning or end of page list length
+		 		if($left_offset < 1){
+		 			$right_offset = min(
+		 				$n_pages,
+		 				($right_offset + abs($left_offset-1))
+		 			);
+		 		}
+		 		if($right_offset > $n_pages){
+		 			$left_offset = max(
+		 				1,
+		 				($left_offset - abs($n_pages-$right_offset))
+		 			);
+		 		}
+
+		 		return array(
+		 			"left" => $left_offset,
+		 			"right" => $right_offset
+		 		);
+		 	}
+
+	 }
 
 ?>
