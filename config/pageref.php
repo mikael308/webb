0e83c4672f9fead1429ecfad592b40afd7486f9d
mikael308 <mikael.holmbom@hotmail.com>
2017-07-06 23:47:32 +0200

    fix indent

--- config/pageref.php
+++ config/pageref.php
@@ -1,17 +1,19 @@
 <?php
-/**
- * meta data:
- * contains global vars: page reference filepaths to pages
- * @author Mikael Holmbom
- */
+  /**
+   * meta data:
+   * contains global vars: page reference filepaths to pages
+   * @author Mikael Holmbom
+   */
+  
+  # references to actual page files
+  $GLOBALS["pageref"] = array(
+    "about"         => "about.php",
+    "admin"         => "admin.php",
+    "contact"       => "contact.php",
+    "forum"         => "forum",
+    "index"         => "index.php",
+    "post"          => "post.php",
+    "register"      => "registeruser.php",
+    "user"          => "user.php"
+  );
 
-	$GLOBALS["index_page"] = "index.php";
-	$GLOBALS["post_page"] = "post.php";
-	$GLOBALS["forum_page"] = "forum.php";
-	$GLOBALS["admin_page"] = "admin.php";
-	$GLOBALS["user_page"] = "user.php";
-	$GLOBALS["about_page"] = "about.php";
-	$GLOBALS["register_page"] = "registeruser.php";
-	$GLOBALS["contact_page"] = "contact.php";
-
-?>
