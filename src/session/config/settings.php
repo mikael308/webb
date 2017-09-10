db3fa8d9eb0eb25c16f69a5b363a34be9e9aacfb
mikael308 <mikael.holmbom@hotmail.com>
2017-09-10 19:53:09 +0200

    mv settings php

---
+++ src/session/config/settings.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ *
+ * helper class to read from settings file\n
+ * settings.md
+ *
+ * @author Mikael Holmbom
+ * @version 1.0
+ */
+
+namespace Session;
+
+class Settings
+{
+    protected $settings_filename = "../../config/settings.conf";
+
+    protected $settingsValues;
+
+	public function __construct()
+	{
+        $this->reset();
+	}
+
+	public function reset()
+    {
+        $this->settingsValues = [];
+
+        $settings_file = fopen($this->settings_filename, "r") or die("unable to read settings");
+
+        while (! feof($settings_file)) {
+            $line = fgets($settings_file);
+
+            $tl = trim($line);
+            if(strlen($tl) == 0 || $tl[0] == "#"){
+                continue;
+            }
+
+            $row_arr = explode(" ", $tl);
+
+            $this->settingsValues["<?= $row_arr[0] ?>"] = $row_arr[1];
+
+        }
+
+        fclose($settings_file);
+    }
+
+    public function value(
+        $settings_key
+    ) {
+	    return $this->settingsValues["$settings_key"];
+    }
+
+    /**
+     * read settings option value from settings file
+     * @param setting_name string name of the requested settings option
+     * @return string settings options value. if settings option was not found, return NULL
+     */
+    public function read($setting_name){
+        $ret_val = NULL;
+        $settings_file = fopen($this->settings_filename, "r") or die("unable to read settings");
+
+        while (! feof($settings_file)) {
+            $line = fgets($settings_file);
+
+            $tl = trim($line);
+            if(strlen($tl) == 0 || $tl[0] == "#"){
+                continue;
+            }
+
+            $row_arr = explode(" ", $tl);
+
+            if($row_arr[0] == $setting_name){
+                $ret_val = $row_arr[1];
+
+            }
+
+        }
+
+        fclose($settings_file);
+
+        return $ret_val;
+    }
+
+}
