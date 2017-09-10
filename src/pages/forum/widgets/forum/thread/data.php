9517f225b8841e3668a0285329923d084c393610
mikael308 <mikael.holmbom@hotmail.com>
2017-09-10 19:39:55 +0200

    move logic to data

--- src/pages/forum/widgets/forum/thread/data.php
+++ src/pages/forum/widgets/forum/thread/data.php
@@ -1,23 +1,63 @@
 <?php
-
 namespace Widgets\Forum\Forum\Thread;
 
 require_once "./database/post.php";
 require_once "./database/Read.php";
 
-#TODO rm
 use \Database\Post;
-use \Database\Read;
 
 class Data extends \Framework\Data\Widget
 {
-	public function getThread()
-	{
-		return \Database\Read::thread(getThreadIndex());
-	}
+    protected $thread;
 
-	public function getPost()
-	{
-		return \Databse\Read::post(getPostIndex());
-	}
+    protected $posts;
+
+    public function __construct()
+    {
+        $this->thread = \Database\Read::thread(getThreadIndex());
+        $this->posts = \Database\Read::postsFromThread($this->thread->getPrimaryKey());
+    }
+
+    public function getThread()
+    {
+        return $this->thread;
+    }
+
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    public function getPost()
+    {
+        return \Database\Read::post(getPostIndex());
+    }
+
+    public function getMaxPosts()
+    {
+        $maxlim = ($this->postsPerPage() + $this->getStartOffset());
+        $n_posts = $this->getThread()->getSize();
+        $maxPosts = min($maxlim, $n_posts);
+
+        return $maxPosts;
+    }
+
+    public function getStartOffset()
+    {
+        $n_postsPerPage = $this->postsPerPage();
+        $start_offset = (getPageIndex() - 1) * $n_postsPerPage;
+
+        return $start_offset;
+    }
+
+    public function postsPerPage()
+    {
+        return \Settings\read("posts_per_page");
+    }
+
+    public function getMaxPages()
+    {
+        return \Database\Count::maxPagesThread($this->thread);
+    }
+
 }
