fdc3b01227a322aae09ccb74525f6f7e12f12540
mikael308 <mikael.holmbom@hotmail.com>
2017-10-29 19:41:10 +0100

    cleanup comments

--- bootstrap/database/setup_database.sql
+++ bootstrap/database/setup_database.sql
@@ -1,15 +1,6 @@
 -- SETUP database
 -- author Mikael Holmbom
 
--- CREATE DATABASE postgres -h 10.0.2.15 -p 5432 -U postgres postgres;
-
 ALTER USER postgres WITH PASSWORD 'password';
 
--- CREATE DATABASE postgres;-- -p 5432 -U postgres -d postgres;
--- CREATE DATABASE ps_database;
 CREATE SCHEMA proj;
-
-
--- CREATE ROLE 'admin' PASSWORD 'admin';
--- GRANT ALL PRIVILEGES ON DATABASE 'ps_database' to admin;
-
