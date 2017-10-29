bbbcf1e322f1d0d32ce1a3a61938d58e7cb86821
mikael308 <mikael.holmbom@hotmail.com>
2017-10-29 17:27:17 +0100

    redo calls

--- src/database/Count.php
+++ src/database/Count.php
@@ -20,27 +20,27 @@ class Count{
     public static function forumThreads(
         ForumUser $user
     ) {
-        $query =
-            " FROM ".$GLOBALS['database']['table']['forumthreads']." AS t "
-            . " LEFT JOIN ".$GLOBALS['database']['table']['forumposts']." as p "
+        $table = $GLOBALS['database']['table']['forumthreads'];
+        $join_table = $GLOBALS['database']['table']['forumposts'];
+                                                                                                                                    
+        return Count::query(
+            " FROM $table AS t "
+            . " LEFT JOIN $join_table as p "
             . " ON p.thread=t.id "
             . " WHERE p.author = '".$user->getPrimaryKey()."' "
-            . " AND p.id IN "
-            . " ( " # the first posts in every thread
+            . " AND p.id IN ( " 
+                # the first posts in every thread
                 . " SELECT p.id "
-                . " FROM ".$GLOBALS['database']['table']['forumposts']." p "
-                . " WHERE p.id = "
-                . " ( "
+                . " FROM $join_table p "
+                . " WHERE p.id =  ( "
                     . " SELECT p2.id "
-                    . " FROM ".$GLOBALS['database']['table']['forumposts']." p2 "
+                    . " FROM $join_table p2 "
                     . " WHERE p2.thread=p.thread "
                     . " ORDER BY p2.created ASC "
                     . " LIMIT 1 "
                 . " ) "
-            . " ) "
-                . " ;";
-                                                                                                                                                    
-        return Count::query($query);
+            . " );"
+        );
     }
     /**
      * count the number of forumposts created by specific user
@@ -50,12 +50,11 @@ class Count{
     public static function forumPosts(
         ForumUser $user
     ) {
-        $query =
-            " FROM " . $GLOBALS['database']['table']['forumposts']
-            . " WHERE author='" . $user->getPrimaryKey() . "'"
-            . " ;";
-
-        return Count::query($query);
+        $table = $GLOBALS['database']['table']['forumposts'];
+        return Count::query(
+            " FROM $table "
+            . " WHERE author='" . $user->getPrimaryKey() . "';"
+        );
     }
 
     /**
@@ -67,16 +66,18 @@ class Count{
     public static function postPageIndex(
         $post_pk
     ) {
-        $query = 
-             " FROM " . $GLOBALS['database']['table']['forumposts']
+        $table = $GLOBALS['database']['table']['forumposts'];
+
+        $count = Count::query(
+            " FROM $table "
             . " WHERE thread='". Read::forumPost($post_pk)->getThread()->getPrimaryKey() ."' "
             . " AND created <= ( "
             .       " SELECT created "
-            .       " FROM " . $GLOBALS['database']['table']['forumposts']
+            .       " FROM $table "
             .       " WHERE id='".$post_pk."' "
-            .   " ) "
-            . " ;";
-        return ceil(Count::query($query) / $_SESSION['settings']->value("posts_per_page"));
+            . " );"
+        );
+        return ceil($count / $_SESSION['settings']->value("posts_per_page"));
     }
     /**
      * count results from database query
@@ -89,8 +90,10 @@ class Count{
     ) {
         $db_conn = connect();
         if($db_conn){
-            $q = "SELECT COUNT(*) " . $query;
-            $res = pg_query($db_conn, $q);
+            $res = pg_query(
+                $db_conn,
+                "SELECT COUNT(*) " . $query
+            );
             if($res){
                 $num = pg_fetch_object($res)->count;
                 pg_free_result($res);
@@ -112,12 +115,12 @@ class Count{
     public static function maxPagesThread(
         ForumThread $thread
     ) {
-        $query =
-            " FROM " . $GLOBALS['database']['table']['forumposts']
-            . " WHERE thread='" . $thread->getPrimaryKey() ."'"
-            .";";
-        $n_posts        = Count::query($query);
-        $max_pages      = ceil($n_posts / $_SESSION['settings']->value("posts_per_page"));
+        $table = $GLOBALS['database']['table']['forumposts'];
+        $n_posts = Count::query(
+            " FROM $table "
+            . " WHERE thread='" . $thread->getPrimaryKey() ."';"
+        );
+        $max_pages = ceil($n_posts / $_SESSION['settings']->value("posts_per_page"));
         return $max_pages;
     
     }
@@ -130,12 +133,12 @@ class Count{
     public static function maxPagesSubject(
         ForumSubject $subject
     ) {
-        $query =
-            " FROM " . $GLOBALS['database']['table']['forumthreads'] . " AS t "
-            . " WHERE t.subject='" . $subject->getPrimaryKey() . "'"
-            . ";";
-
-        $n_threads = Count::query($query);
+        $table = $GLOBALS['database']['table']['forumthreads'];
+        
+        $n_threads = Count::query(
+            " FROM $table AS t "
+            . " WHERE t.subject='" . $subject->getPrimaryKey() . "';";
+        );
         $threads_per_page = $_SESSION['settings']->value("threads_per_page");
         $max_pages = ceil($n_threads / $threads_per_page);
 
@@ -149,12 +152,11 @@ class Count{
     public static function postsBy(
         ForumThread $thread
     ) {
-        $query =
-            " FROM " . $GLOBALS['database']['table']['forumposts'] . " AS p "
-            . " WHERE p.thread='".$thread->getPrimaryKey()."'"
-            . ";";
-
-        return Count::query($query);
+        $table = $GLOBALS['database']['table']['forumposts'];
+        return Count::query(
+            " FROM $table AS p "
+            . " WHERE p.thread='".$thread->getPrimaryKey()."';"
+        );
     }
 
 } # ! COUNT
