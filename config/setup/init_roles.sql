16cbb90936fd3b2ab67c4dd3f05ed5a59efd0bae
mikael308 <mikael.holmbom@hotmail.com>
2017-10-30 22:12:03 +0100

    rm config/setup

--- config/setup/init_roles.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO proj.roles(id, title)
-VALUES
-(0, 'admin'),
-(1, 'moderator'),
-(2, 'user');
--- config/setup/setup.sh
+++
@@ -1,8 +0,0 @@
-psql -U mikael -d postgres -a -f ./config/setup/setup_tables.sql
-psql -U mikael -d postgres -a -f ./config/setup/init_roles.sql
-
-php ./config/setup/setup_users.php
-
-psql -U mikael -d postgres -a -f ./config/setup/init_vals.sql
-psql -U mikael -d postgres -a -f ./config/setup/setup_storedprocedures.sql
-php ./config/setup/setup_forumposts.php
