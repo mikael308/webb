4e37842f0e38807234960fa446831107b60fcff0
mikael308 <mikael.holmbom@hotmail.com>
2017-11-18 11:41:27 +0100

    rm create db from setup_db script

--- setup/setup_database.sh
+++ setup/setup_database.sh
@@ -6,16 +6,3 @@
 
 sudo apt-get -y install postgresql postgresql-contrib postgresql-client postgresql-client-common
 sudo service apache2 restart
-
-sudo su postgres <<SHELL
-echo "setup_database.sql"
-psql -U postgres -d postgres -f /vagrant/setup/database/setup_database.sql
-echo "setup_tables.sql"
-psql -U postgres -d postgres -f /vagrant/setup/database/setup_tables.sql
-echo "setup_storedprocedures.sql"
-psql -U postgres -d postgres -f /vagrant/setup/database/setup_storedprocedures.sql
-
-# init values
-psql -U postgres -d postgres -f /vagrant/setup/database/init_roles.sql
-
-SHELL
