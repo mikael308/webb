7b1acc320f8dbd3117ded74dfa4d8025f2439c16
mikael308 <mikael.holmbom@hotmail.com>
2017-11-20 00:09:50 +0100

    update ajax call

--- src/ajax/searchres.php
+++ src/ajax/searchres.php
@@ -2,130 +2,55 @@
 namespace Web\Ajax;
 
 /**
- * use request 'id' for searchstring \n
- * echos list of links to users that match with searchstring\n
- * max amount of results displayed set in settings.md as var searchres_user_amount
+ * use request 'value' for searchstring <br>
+ * use request 'type' for searchtype<br>
  * @author Mikael Holmbom
  * @version 1.0
  */
+
+use function Web\pagelinkForumThread;
+use function Web\pagelinkPost;
+use function Web\pagelinkUser;
+
 header('Content-Type: application/json');
 
-#require_once "./reference.php";
-#require_once "./config/settings.php";
-#require_once "./webb/src/database/database.php";
-#require_once "../database/database.php";
-#$serverroot = $_SERVER['DOCUMENT_ROOT'];
-#$databasePath = "$serverroot/webb/src/database/database.php";
-#require_once $_SERVER['DOCUMENT_ROOT'] ."/webb/src/database/database.php";
-#require_once $_SERVER['DOCUMENT_ROOT'] ."/webb/src/database/search.php";
-#require_once "./sections/forum/main.php";
+require_once '/vagrant/src/database/search.php';
+require_once '/vagrant/src/framework/format.php';
 
-#TODO denna fil hittas. men den kraschar.. istllet för att fixa alla elements här i
-# returnera en JSON bara och skriv ut den i searchsidepanel... bara bra om den fixar all frontend...
+$searchstr = $_REQUEST['value'] ?? '';
+$searchType = $_REQUEST['type'] ?? '';
 
-echo "<p>searchres ajax found</p>";
-/**
- *
- * search result for ForumPost type
- * @param post \Database\DAO\ForumPost the search result
- * @return string searchresult as html
- *//*
-function searchresPost(
-    \Database\DAO\ForumPost $post
-) {
-    $maxlen = 40;
-    $label = \Helper\Format::textToLength($post->getMessage(), $maxlen);
-    $p = \Database\Count::postPageIndex($post->getPrimaryKey());
-    $t = $post->getThread();
-    $author = $post->getAuthor();
-    return
-        "<a href='". pagelinkForumThread($t, $p) ."'>"
-        .   "<div class='post main'>"
-        .     $label
-        .   "</div>"
-        . "</a>"
-        . "<div class='post extra'>"
-        .   "<div class='author'>"
-        .     "author: <a href='". pagelinkUser($author) ."'>"
-        .       $author->getName() . "</a>"
-        .   "</div>"
-        .   "<div class='topic' >"
-        .     "topic: <a href='".pagelinkForumThread($t, 1)."'>"
-        .       $t->getTopic() ."</a>"
-        .   "<div>"
-        . "</div>";
-}*/
+$suggest = [
+    'status'    => 'NOFOUND'
+];
+$resArray = [];
+if ($searchstr != '') {
 
-/**
- *
- * search result for ForumUser type
- * @param user 
- * @return string result as html
- *//*
-function searchresForumUser(
-    \Database\DAO\ForumUser $user
-) {
-    $cont = "<div class='name'>". $user->getName() ."</div>"
-        . "<div class='role'>". $user->getRole() . "</div>";
-    return
-            "<a href='" . pagelinkUser($user) . "' class='user main'>"
-        . "<div class='user extra'>"
-        .   $cont
-        . "</div>"
-        . "</a>"
-        ;
-}*/
-
-/**
- * get data as searchres listitem
- * @param data string listitem data
- * @return string li as html
- *//*
-function search_li(
-    $data
-) {
-    return "<li class='searchres_item'>". $data ."</li>";
-}*/
-
-$searchstr = isset($_REQUEST["value"]) ? $_REQUEST["value"] : "";
-$searchType = isset($_REQUEST["type"]) ? $_REQUEST["type"] : "";
-
-$nothingfoundres = "<li class='searchres_item'>no results</li>";
-$suggest = "";
-if ($searchstr != "") {
-    $reslist = "";
-
-    $json_array = [];
-    # determine the type of the search
     switch($searchType) {
-        case "post":
-            $reslist = \Web\Database\searchPost($searchstr);
-
-            /*$reslist = \Database\searchPost($searchstr);
-            if($reslist == NULL){
-                $suggest = $nothingfoundres;
-            } else {
-                foreach($reslist as $item){
-                    $suggest .= search_li(searchresPost($item));
-                }
-            }*/
+        case 'post':
+            $objArray = \Web\Database\searchPost($searchstr);
+            foreach ($objArray as $obj) {
+                $arr = $obj->toArray();
+                $thread = $obj->getThreaD();
+                $arr['label'] = \Web\Framework\Format::textToLength($obj->getMessage(), 20);
+                #$arr['link'] = pagelinkForumThread($obj->getThreadFK(), $obj->getPageIndex());
+                $arr['topic'] = $thread->getTopic();
+                $arr['author_name'] = $obj->getAuthor()->getName();
+                $resArray[] = $arr;
+            }
             break;
-        case "user":
-            $reslist = \Web\Database\searchForumUser($searchstr);
-
-            /*if($reslist == NULL){
-                $suggest = $nothingfoundres;
-            } else {
-                foreach($reslist as $item){
-                    $suggest .= search_li(searchresForumUser($item));
-                }
-            }*/
-        break;
+        case 'user':
+            $objArray = \Web\Database\searchForumUser($searchstr);
+            foreach ($objArray as $obj) {
+                $resArray[] = $obj->toArray();
+            }
+            break;
     }
-    $json_array = json_encode($reslist);
-    $suggest = $json_array;
+    if (sizeof($resArray) > 0)
+        $suggest['status'] = 'OK';
 }
 
-echo $suggest == ""
-    ? ""
-    : $suggest;
+$suggest['items'] = $resArray;
+
+$json_array = json_encode($suggest);
+echo $json_array;
